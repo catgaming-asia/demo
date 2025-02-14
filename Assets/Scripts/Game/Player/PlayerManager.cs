4689b68916f2ccbc98c5a1f9292b0339f74258fe
phu tran <phutranquang2000@gmail.com>
2025-02-14 15:00:52 +0700

    Working: move with direction

--- Assets/Scripts/Game/Player/PlayerManager.cs
+++ Assets/Scripts/Game/Player/PlayerManager.cs
@@ -21,7 +21,7 @@ namespace Game.Player
 
         private bool _isOwner;
         private Action _moveAction;
-
+        private Vector3 _direction;
         public NetworkVariable<Vector3> Position = new NetworkVariable<Vector3>(
             writePerm: NetworkVariableWritePermission.Owner);
 
@@ -40,66 +40,6 @@ namespace Game.Player
             this.transform.localScale = Vector3.one;
             this.transform.localPosition = new Vector3(0, 0, 0);
         }
-
-        // private void Update()
-        // {
-        //     if (_isOwner)
-        //     {
-        //         if (Input.GetMouseButton(0))
-        //         {
-        //             Vector3 mousePos = Input.mousePosition;
-        //
-        //             _direction = (mousePos - transform.position).normalized;
-        //             float distanceToMouse = Vector3.Distance(transform.position, mousePos);
-        //
-        //
-        //             if (distanceToMouse - stopDistance < 0)
-        //             {
-        //                 return; // Stop movement
-        //             }
-        //
-        //             if (_direction != Vector3.zero)
-        //             {
-        //                 float angle = Mathf.Atan2(-_direction.x, _direction.y) * Mathf.Rad2Deg;
-        //                 transform.rotation = Quaternion.Euler(0, 0, angle);
-        //             }
-        //         }
-        //
-        //         Vector3 moveDirection = Vector3.zero;
-        //         if (Input.GetKey(KeyCode.W))
-        //         {
-        //             moveDirection += _direction;
-        //         }
-        //
-        //         if (Input.GetKey(KeyCode.S))
-        //         {
-        //             moveDirection -= _direction;
-        //         }
-        //
-        //         if (Input.GetKey(KeyCode.A))
-        //         {
-        //             moveDirection += new Vector3(-_direction.y, _direction.x, 0);
-        //         }
-        //
-        //         if (Input.GetKey(KeyCode.D))
-        //         {
-        //             moveDirection += new Vector3(_direction.y, -_direction.x, 0);
-        //         }
-        //
-        //         Vector3 newPosition = transform.position + moveDirection.normalized * speed;
-        //         newPosition.x =
-        //             Mathf.Clamp(newPosition.x, _bottomLeftPosition.x + stopDistance,
-        //                 _bottomRightPosition.x - stopDistance);
-        //         newPosition.y =
-        //             Mathf.Clamp(newPosition.y, _bottomLeftPosition.y + stopDistance,
-        //                 _topLeftPosition.y - stopDistance);
-        //         transform.position = newPosition;
-        //     }
-        //     else
-        //     {
-        //         transform.position = Position.Value;
-        //     }
-        // }
 
         public override void OnNetworkSpawn()
         {
@@ -132,39 +72,58 @@ namespace Game.Player
                 if (Input.GetMouseButton(0)) // Check if the left mouse button is held
                 {
                     Vector3 mousePos = Input.mousePosition;
-                    Vector3 direction = (mousePos - transform.position).normalized;
+                    _direction= (mousePos - transform.position).normalized;
                     float distanceToMouse = Vector3.Distance(transform.position, mousePos);
                     if (distanceToMouse <= stopDistance)
                     {
                         return; 
                     }
-                    if (direction != Vector3.zero)
+                    if (_direction != Vector3.zero)
                     {
-                        float angle = Mathf.Atan2(-direction.x, direction.y) * Mathf.Rad2Deg;
+                        float angle = Mathf.Atan2(-_direction.x, _direction.y) * Mathf.Rad2Deg;
                         transform.rotation = Quaternion.Euler(0, 0, angle);
                     }
                 }
-                if (Input.GetKey(KeyCode.A))
-                {
-                    CachedPosition = Movement(TypeMove.left);
-                    Move();
-                }
+                Vector3 moveDirection = Vector3.zero;
+                // if (Input.GetKey(KeyCode.A))
+                // {
+                //     CachedPosition = Movement(TypeMove.left);
+                //     Move();
+                // }
+                //
+                // if (Input.GetKey(KeyCode.D))
+                // {
+                //     CachedPosition = Movement(TypeMove.right);
+                //     Move();
+                // }
+                //
+                // if (Input.GetKey(KeyCode.W))
+                // {
+                //     CachedPosition = Movement(TypeMove.forward);
+                //     Move();
+                // }
+                //
+                // if (Input.GetKey(KeyCode.S))
+                // {
+                //     CachedPosition = Movement(TypeMove.backward);
+                //     Move();
+                // }
+                
+                if (Input.GetKey(KeyCode.W)) // Move forward (toward mouse)
+                    moveDirection += _direction;
 
-                if (Input.GetKey(KeyCode.D))
-                {
-                    CachedPosition = Movement(TypeMove.right);
-                    Move();
-                }
+                if (Input.GetKey(KeyCode.S)) // Move backward (away from mouse)
+                    moveDirection -= _direction;
 
-                if (Input.GetKey(KeyCode.W))
-                {
-                    CachedPosition = Movement(TypeMove.forward);
-                    Move();
-                }
+                if (Input.GetKey(KeyCode.A)) // Move left (perpendicular to mouse direction)
+                    moveDirection += new Vector3(-_direction.y, _direction.x, 0);
 
-                if (Input.GetKey(KeyCode.S))
+                if (Input.GetKey(KeyCode.D)) // Move right (perpendicular to mouse direction)
+                    moveDirection += new Vector3(_direction.y, -_direction.x, 0);
+
+                if (moveDirection != Vector3.zero)
                 {
-                    CachedPosition = Movement(TypeMove.backward);
+                    CachedPosition = moveDirection.normalized * speed * Time.deltaTime;
                     Move();
                 }
             }
